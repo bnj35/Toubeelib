e23f3e3e656aea3aea88d292dce2ae5efdbe04a2
bnj35 <benjamin.auger6@etu.univ-lorraine.fr>
2025-01-14 10:36:10 +0100

    :art:/update structure de code

--- gateway/src/core/services/patient/AuthorizationPatientServiceInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace toubeelib\core\services\patient;
-
-interface AuthorizationPatientServiceInterface
-{
-    public function isGranted(string $user_id, int $operation, string $ressource_id, int $role): bool;
-
-}
